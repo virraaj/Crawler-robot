bdc4213c3de5d173680e53923e5d40508e2a1807
viraj <virajshah.vs@gmail.com>
2018-08-07 17:03:40 +0200

    encoder

---
+++ KY040/examples/python3.py
@@ -0,0 +1,31 @@
+from time import sleep
+import RPi.GPIO as GPIO
+from ky040.KY040 import KY040
+
+
+CLOCKPIN = 17
+DATAPIN = 18
+SWITCHPIN = 13
+
+
+def rotaryChange(direction):
+    print("turned - " + str(direction))
+
+
+def switchPressed():
+    print("button pressed")
+
+
+GPIO.setmode(GPIO.BCM)
+
+ky040 = KY040(CLOCKPIN, DATAPIN, SWITCHPIN, rotaryChange, switchPressed)
+
+ky040.start()
+
+try:
+    while True:
+        sleep(0.1)
+        #a = 1
+finally:
+    ky040.stop()
+    GPIO.cleanup()
