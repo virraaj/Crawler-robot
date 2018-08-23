90b93220a67c43c10604cbfdab9f6329532190db
Mihir <mihir.shah@hs-weingarten.de>
2018-08-23 16:47:51 +0200

    added temp file of ValueIteration_temp5

--- test.py
+++ test.py
@@ -1,8 +1,8 @@
-import pinSetup
+import pinSetup_TEMPencoder
 import time
 import gotopos
 import action_20
-pinVar = pinSetup.pinSetup()
+pinVar = pinSetup_TEMPencoder.pinSetup()
 p = pinVar[0]
 p1 = pinVar[1]
 encoder = pinVar[2]
@@ -10,7 +10,6 @@ ENClast = pinVar[3]
 p.start(4.25)
 p1.start(10.0)
 time.sleep(0.1)
-'''
 for x in range(0, 200):
     # y = mx+c
     c = 3.0
@@ -18,11 +17,3 @@ for x in range(0, 200):
     y = m * x + c
     p1.ChangeDutyCycle(y)
     time.sleep(0.05)
-'''
-# gotopos.gotopos(2,2,p,p1, 3)
-# time.sleep(0.1)
-# ENClast = encoder.getData()
-# action_20.playAction(2,2,2,3,p,p1)
-# time.sleep(0.1)
-# ENC = encoder.getData()
-# print (ENC - ENClast)
