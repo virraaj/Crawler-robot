c3d7171db273c45dc6555ba6a4545e28b912b833
viraj <virajshah.vs@gmail.com>
2018-08-30 13:15:07 +0200

    modified qinitial

--- qinitial.py
+++ qinitial.py
@@ -1,7 +1,11 @@
 def qinitial(n):
 
-    value = [[[0]*4]*n for i in range(n)]
+    #value = [[[0]*4]*n for i in range(n)]
     #action = [[None]*n for i in range(n)]
+    a = [0,0,0,0]
+    for i in range(1,n+1):
+        for j in range(1,n+1):
 
+            value = [[a]*j]*i
     return value
 print qinitial(3)
